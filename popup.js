0102dd9aa25229a0835a9d4a7558e143770869f4
vickydoosa <vickydoosa@gmail.com>
2015-09-01 15:10:11 -0700

    Added to Git

---
+++ popup.js
@@ -0,0 +1,141 @@
+defaults = {
+  url : 'https://secure.splitwise.com/api/v3.0/get_printable_summary',
+  errorMsg : '<span class="glyphicon glyphicon-remove"></span> '
+    + 'App does not support this page',
+  otherErrorMsg : '<span class="glyphicon glyphicon-thumbs-down"></span> '
+    + 'Something went wrong. Please try again',
+  round: function(num) {
+    return Math.round(num * 100) / 100;
+  },
+  // http://stackoverflow.com/questions/17563677/convert-javascript-number-to-currency-format-but-without-or-any-currency-sym
+  regex: /(\d)(?=(\d{3})+(?!\d))/g,
+  amtToStr: function(num) {
+    return '$' + (this.round(Math.abs(num)) + '')
+      .replace(this.regex, "$1,");
+  }
+};
+
+chrome.extension.onMessage.addListener(function(request, sender) {
+  if (request.action == "getSource") {
+    // message.innerText = request.source;
+    var data = request.source;
+    console.log(data);
+    loadData(data);
+  }
+});
+
+function onWindowLoad() {
+  
+  //
+  $.fn.switchClass = function(a, b) {
+    if (this.hasClass(a))
+      this.removeClass(a).addClass(b);
+    return this;
+  };
+  
+  chrome.tabs.getSelected(function(tab) {
+    
+    if (tab.url.indexOf(defaults.url) === -1) {
+      $('#message').html(defaults.errorMsg).show();
+      return;
+    }
+    // inject the script
+    chrome.tabs.executeScript(null, {
+      file: "quick.js"
+    }, function() {
+      if (chrome.extension.lastError) {
+        message.innerText = 'There was an error injecting script : \n' 
+          + chrome.extension.lastError.message;
+      }
+    });
+    
+  });
+}
+
+function loadData(obj) {
+  
+  var $tb,
+    $info,
+    all,
+    $tr;
+  
+  if (!obj) {
+    $('#message').html(defaults.otherErrorMsg).show();
+    return;
+  }
+  
+  // set info
+  $info = $('#info');
+  $info.append($('<h3>').text(obj.info.group));
+  
+  if (obj.info.time)
+    $info.append($('<h4>', {
+      'html' : '<small><span class="glyphicon glyphicon-time"></span></small> ' 
+      + obj.info.time}));
+  
+  // add table
+  $tb = $('<tbody>');
+  all = obj.people.all;
+  
+  if (!all || all.length === 0) {
+    $('#message').html(defaults.otherErrorMsg).show();
+    return;
+  }
+  
+  for(var i = 0, len = all.length; i < len; i++) {
+    $tr = $('<tr>');
+    amt = 0;
+    // name
+    $td = $('<td>').text(all[i]['name']);
+    $tr.append($td);
+    
+    // expense
+    $tr.append(getAmtEle(all[i]['spend']['expense'])
+      .switchClass('text-success', 'text-primary'));
+    
+    // borrowed
+    $tr.append(getAmtEle(-all[i]['spend']['borrowed']));
+    
+    // Lent
+    $tr.append(getAmtEle(all[i]['spend']['lent']));
+    
+    // Paid
+    $tr.append(getAmtEle(all[i]['spend']['paid']));
+    
+    // Received
+    $tr.append(getAmtEle(-all[i]['spend']['received']));
+    
+    // Owe before
+    $tr.append(getAmtEle(all[i]['wasOwedBefore']));
+    
+    // Total owe
+    $tr.append(getAmtEle(all[i]['spend']['totalPaid'] 
+      - all[i]['spend']['totalShare'] + all[i]['wasOwedBefore']));
+    
+    $tb.append($tr);
+  }
+  
+  
+  $('#main-table-body').replaceWith($tb);
+  $('#main').removeClass('hidden');
+  
+}
+
+function formatEle($ele, amt) {
+  var type = amt < 0 ? false : true,
+    str = (amt !== 0 && !type ? '-' : '') + defaults.amtToStr(amt),
+    className = (amt !== 0 && !type ? 'text-danger' : 
+      (amt === 0 ? 'text-muted' : 'text-success'));
+  
+  $ele.text(str);
+  $ele.addClass(className + ' amt');
+}
+
+function getAmtEle(amt) {
+  var $td = $('<td>');
+  formatEle($td, amt);
+  
+  return $td;
+}
+
+window.onload = onWindowLoad;
---
+++ quick.js
@@ -0,0 +1,205 @@
+
+
+(function() {
+
+var defaults = {
+  strToAmtReg : /\$(\d+)/,
+  // http://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-in-javascript
+  round: function(num) {
+    return Math.round(num * 100) / 100;
+  }
+};
+
+function getData(d, w) {
+  
+  var grpData = d.querySelector(".group_summary"),
+    people,
+    pData = d.querySelectorAll(".user_summary"),
+    data = {},
+    info = {},
+    isMonth = false,
+    peopleIndex = 3,
+    dataDate,
+    userData;
+    
+  info['group'] = grpData.getElementsByTagName('h2')[0].textContent;
+  dataDate = grpData.getElementsByTagName('h4')[0];
+  
+  if (dataDate) {
+    info['time'] = dataDate.textContent;
+    isMonth = true;
+    peopleIndex = 1;
+  }
+  
+  data['info'] = info;
+  
+  people = getPeople(grpData.querySelector('table'), peopleIndex, isMonth);
+  data['people'] = people;
+  
+  if (pData.length == people.all.length) {
+    // get people data
+    for (var i = 0; i < pData.length; i++) {
+      userData = getUserData(pData[i].querySelector('table'));
+      people.all[i]['spend'] = userData;
+    }
+    
+  }
+  
+  console.log(data);
+  
+  return data;
+}
+
+function getUserData(t) {
+  // Get the user data
+  var tot = t.querySelectorAll('.total td'),
+    trs = t.querySelectorAll('.expense'),
+    tds,
+    data = {
+      'totalPaid' : 0,
+      'totalShare' : 0,
+      'lent' : 0,
+      'borrowed' : 0,
+      'received': 0,
+      'paid': 0,
+      'expense': 0
+    },
+    obj;
+    
+    data['totalPaid'] = strToAmt(tot[1].textContent);
+    data['totalShare'] = strToAmt(tot[2].textContent);
+  
+  
+  for (var i = 0; i < trs.length; i++) {
+    tds = trs[i].getElementsByTagName('td');
+    obj = getBalance(tds[5].textContent);
+    if (obj && obj['type'])
+      data[obj['type']] += obj['amt'];
+  }
+  
+  // caluculate expense = totalShare - received
+  data['expense'] = data['totalShare'] - data['received'];
+  
+  return data;
+  
+}
+
+function getPeople(t, index, isOwed) {
+  var p = {},
+    ts,
+    man,
+    name,
+    oweAmt;
+    
+  p.p = {};
+  p.all = [];
+    
+  if (!t)
+    return p;
+    
+  
+  ts = t.querySelectorAll('tr th');
+  
+  if (ts.length === 0)
+    return p;
+    
+  for(var i = index; i < ts.length; i++) {
+    name = ts[i].childNodes[0].textContent.trim();
+    man = {
+      'name' : name,
+      'wasOwedBefore' : 0
+    };
+    
+    p.p[name] = p.all.push(man) - 1;
+    if (isOwed)
+      oweAmt = getOwedAmt(ts[i].querySelector('div'));
+    else
+      oweAmt = 0;
+      
+    man['wasOwedBefore'] = oweAmt;
+    
+  }
+  
+  return p;
+}
+
+function getOwedAmt(div) {
+  if (!div)
+    return 0;
+    
+  var cl = div.getAttribute('class'),
+    txt = div.innerText.trim(),
+    oweAmt = 0,
+    mult = 1;
+  
+  switch(cl) {
+    case 'settled':
+      return 0;
+    case 'negative':
+      mult = -1;
+      break;
+  }
+  
+  try {
+    oweAmt = defaults.round(parseFloat(txt.replace(/.+ \$(.+)$/, '$1'))) * mult;
+  } catch(e) {}
+  
+  return oweAmt;
+}
+
+function strToAmt(str) {
+  str = str.trim();
+  var amt = parseFloat(str.replace(defaults.strToAmtReg, '$1'));
+  if (isNaN(amt))
+    return 0;
+  
+  return defaults.round(amt);
+}
+
+function getBalance(str) {
+  
+  if (!str || str.trim() === '')
+    return false;
+    
+  str = str.trim();
+  
+  if (str === 'no balance')
+    return false;
+
+  var arr = str.split(' ');
+  
+  if (arr.length !== 3 && arr[0] !== 'you')
+    return false;
+  
+  var data = {
+      'type' : false,
+      'amt' : 0
+    }, 
+    mult = 0;
+  
+  data['amt'] = strToAmt(arr[2]);
+  
+  switch(arr[1]) {
+    case 'received':
+    case 'borrowed':
+      mult = 1;
+      break;
+    case 'paid':
+    case 'lent':
+      mult = 1;
+      break;
+  }
+  
+  if (mult) {
+    data['type'] = arr[1];
+    data['amt'] *= mult;
+  }
+  
+  return data;
+}
+
+chrome.extension.sendMessage({
+    action: "getSource",
+    source: getData(document, window)
+});
+})();
